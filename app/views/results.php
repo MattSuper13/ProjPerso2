69bb8d9c5932043839f1559b172052a5e0a2875b
MattSuper13 <matteo.sciacca@etu.univ-amu.fr>
2026-01-26 17:08:03 +0100

    Favicon path fix 2

--- app/views/results.php
+++ app/views/results.php
@@ -3,7 +3,7 @@
 <head>
     <title>Search results</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" type="image/png" href="/../../images/logo_favicon.png"> <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/../../public/images/logo_favicon.png"> <!-- Favicon -->
 </head>
 <body>
 
